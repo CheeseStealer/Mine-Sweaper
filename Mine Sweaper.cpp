524d1566dcc0add58ada47ccf25829edea31e695
CheeseStealer <hkpwill@gmail.com>
2021-06-18 13:58:33 +1200

    a

--- Mine Sweaper.cpp
+++ Mine Sweaper.cpp
@@ -11,7 +11,7 @@ int main(){
 	int mine_PositionY = 0;
 	int mine_Count = 1;
 	int location[10][10];
-	int mine_Y_Axis[6];
+	int mine_Y_Axis[6];//comment
 	int mine_X_Axis[6];
 	for (int i = 0; i < 10; i++){
 		for (int j = 0; j < 10; j++){
